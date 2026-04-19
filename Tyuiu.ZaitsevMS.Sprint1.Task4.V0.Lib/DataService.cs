b01faa40e42711228c661f0c8e8dde69995f1ee5
Macjim202 <macjim202@gmail.com>
2026-04-19 18:28:13 +0500

    Task4

---
+++ Tyuiu.ZaitsevMS.Sprint1.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.ZaitsevMS.Sprint1.Task4.V0.Lib
+{
+    public class DataService : ISprint1Task3V0
+    {
+        public double Calculate(double x, double y)
+        {
+            var res = 1 / (Math.Pow(x, 2) + Math.Pow(y, 2));
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ZaitsevMS.Sprint1.Task4.V0/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZaitsevMS.Sprint1.Task4.V0.Lib;
+
+namespace Tyuiu.ZaitsevMS.Sprint1.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Зайцев М. С. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Класс Math                                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнил: Зайцев Максим Сергеевич | ИБКСб-25-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x, y;
+
+            Console.WriteLine("Введите значение x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("1 / (x^2 - y^2) = " + ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
